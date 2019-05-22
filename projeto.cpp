2961a24c57637703585736f697a01d7e1eb51692
hun73d <hugofigueira40@gmail.com>
2019-05-21 23:52:21 -0300

    Add files via upload
    Adicionado remoção de registros SEM TRATAMENTO DE COLISÃO (se houver registros de chaves diferentes mas com mesmo hash na mesma lista encadeada, poderá ocorrer a permanência de "resíduo" (pensando em forma de contornar este problema))

--- projeto.cpp
+++ projeto.cpp
@@ -190,8 +190,6 @@ int inserirRegistro(fstream &arquivo, Registro *reg){
 		//Valor a ser substituído
 		return 1;
 }
-/*
-ALTERAR O QUE ESTIVER DAQUI PARA BAIXO
 
 
 
@@ -200,37 +198,42 @@ ALTERAR O QUE ESTIVER DAQUI PARA BAIXO
 
 
 
-//Remove registro
-int removerRegistro(fstream &arquivo, unsigned int key){
-		Registro *buffer = new Registro(0,0,VAZIO, "");
+//Protótipo 1 (apaga mesmo os ponteiros anterior e proximo) -> para casos onde não há conflito de chaves
+void removerRegistro(fstream &arquivo, const char* chave){
+		Registro *buffer = new Registro("", "", -1, -1, VAZIO);
 
-		for(int i = 0; i < TAMANHO; i++){
-				arquivo.seekg(HEADER_OFFSET + hashingDuplo(key,i,TAMANHO)*sizeof(Registro)); //Busca posição
+		unsigned long long int hash = HashString(chave);
+		unsigned long long int hash_swap;
+		do{
+			arquivo.seekg(HEADER_OFFSET + hash*sizeof(Registro));
+			arquivo.read((char*)buffer, sizeof(Registro));
+			arquivo.sync();
+			hash_swap = hash;  //Salva o valor atual do hash para apagar registro;
+			hash = buffer->proximo;	
+			if(!strcmp(buffer->chave, chave)){ //Se a chave na posição for igual a que estamos procurando
 				
-				arquivo.read((char*)buffer, sizeof(Registro)); //Lê registro na posição 
-				arquivo.sync(); //Sincroniza stream com buffer;
 
-				//Caso encontre a chave, apaga o registro;
-				if( (buffer->chave == key) && (buffer->status == OCUPADO)){
-					//A string abaixo foi a maneira encontrada para apagar a string anterior (problemas com header <string>);
-					char string_clean[20] = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"; 
-					
-					arquivo.seekp(-sizeof(Registro),ios_base::cur);  //Recua em 1 posição (o .read anterior avançou 1 posição além)
-					buffer->chave = 0;
-					buffer->idade = 0;
-					
-					strncpy(buffer->nome, string_clean,20);
-					buffer->status = DELETADO;
-					arquivo.write((char*)buffer, sizeof(Registro));
-					arquivo.flush();
-					delete buffer;
-					return 1; //1 indica que apagou com sucesso;
-				}
-		}
+				string clean_chave (20,'\x00');
+				string clean_valor(50,'\x00');
+				strcpy(buffer->chave,clean_chave.c_str());
+				strcpy(buffer->valor,clean_valor.c_str());
+			
+				buffer->anterior = -1;
+				buffer->proximo = -1;
+				buffer->status = VAZIO;
+
+				//Apagando registro em arquivo;
+				arquivo.seekp(HEADER_OFFSET + hash_swap*sizeof(Registro));
+				arquivo.write((char*)buffer, sizeof(Registro));
+				arquivo.flush();
+			}
+			
+		}while(hash != -1);
+
+
 	delete buffer;
-	return 0; //0 indica que não encontrou a chave;
 }
-*/
+
 
 void consultarRegistro(fstream &arquivo, const char* chave){
 		Registro *buffer = new Registro("","",-1,-1 ,VAZIO);
@@ -256,7 +259,7 @@ void consultarRegistro(fstream &arquivo, const char* chave){
 
   //APAGUE ESTE MÉTODO (É SOMENTE PARA DEBUG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!);
 //Exibe todos os registros
-/*void exibirRegistros(fstream &arquivo){
+void exibirRegistros(fstream &arquivo){
 	Registro* buffer = new Registro("","",-1,-1,VAZIO);
 
 
@@ -276,7 +279,7 @@ void consultarRegistro(fstream &arquivo, const char* chave){
 		cout << "status: " << buffer->status << endl;
 	}while(arquivo.tellg()!= tamanho);
 	delete buffer;
-}*/
+}
 
 
 
@@ -310,6 +313,9 @@ int main(int argc, char* argv[]){
  * Se o arquivo tiver tamanho 0, considera-se como novo e será necessário armazenar as estruturas (blocos de registros vazios)
 */
 
+
+
+//AJUSTAR ESTE TRECHO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	int tamanho = 0;
 	arquivo.seekg(0,ios_base::end); //Avança proximo até o último byte para que seja possível "medir" o arquivo;
 	tamanho = arquivo.tellg();
@@ -381,9 +387,13 @@ int main(int argc, char* argv[]){
 						  }
 			//////////////////////////////////////////////////////////////////////////////////////			  
 				case 'r':{
-							//cout << "  Opcao r remover." << endl;
-							cin >> regist->chave;
-							//removerRegistro(arquivo,regist->chave)
+							cin.getline(entrada,100);
+							if(cin.gcount() < 20){   //se forem < (20 caracteres + '\n')
+								strncpy(regist->chave, entrada, cin.gcount());  
+							}else{												  
+								strncpy(regist->chave,entrada, 20);	  //Caso o nome tenha mais de 20 caracteres, pega
+							}		
+							removerRegistro(arquivo,regist->chave);
 							break;	
 						}
 			//////////////////////////////////////////////////////////////////////////////////////			
@@ -400,11 +410,11 @@ int main(int argc, char* argv[]){
 						}
 
 					//DEBUG - REMOVER DEPOIS
-					/*case 'p':{  //Opcao de print dos registros;
+					case 'p':{  //Opcao de print dos registros;
 							exibirRegistros(arquivo);
 							cout << "//////////////////////////////////////////" << endl;
 							break;	
-						}*/
+						}
 			//////////////////////////////////////////////////////////////////////////////////////			
 				case 'e':{ //Opcao de fim de programa;
 							break;	
