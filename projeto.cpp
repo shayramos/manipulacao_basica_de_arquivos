e48fd693b81e1ac59aaa1a00bfc2b18b8ea40db7
hun73d <hugofigueira40@gmail.com>
2019-05-22 17:39:17 -0300

    Add files via upload
    Corrigido bugs na remoção e inserção; Falta realizar mais testes, inserir leitura de arquivo .txt, organizar comentários e se possível otimizar código.

--- projeto.cpp
+++ projeto.cpp
@@ -28,8 +28,10 @@ unsigned long long int HashString(const char* texto){
 } 
 
 
-
-int inserirRegistro(fstream &arquivo, Registro *reg){
+//O bool 'swt' (switch) serve para alternar quando deve verificar a partir do final ou do início do arquivo por uma posição vazia;
+//A função 'inserirRegistro' ficou um tanto grande e com parte de código que podia ser reduzido à funções, mas por falta de tempo
+//não conseguimos reorganizá-la para tal;
+void inserirRegistro(fstream &arquivo, Registro *reg, bool swt){
 
 		//Calcula a posição ideal da chave de reg;
 		unsigned long long int hash_calculado = HashString(reg->chave);
@@ -47,7 +49,7 @@ int inserirRegistro(fstream &arquivo, Registro *reg){
 				unsigned long long int hash_local = HashString(buffer->chave);
 
 				if(hash_local == hash_calculado){ //Se a posição atual for dona do lugar
-					long long int i, index, pos_buffer, pos_anterior;
+					long long int i, index_next, pos_buffer, pos_anterior;
 
 
 					//Vai seguindo a lista encadeada enquanto não encontrar proximo lambda (-1);
@@ -71,14 +73,45 @@ int inserirRegistro(fstream &arquivo, Registro *reg){
 
 					//Esse FOR servirá para ir varrendo o arquivo do fim até o início na tentativa de
 					//encontrar um espaço vazio
+                if(!swt){  //Se o swt (switch) for '0', faz do final pro início;   
 					for(i = TAMANHO; i >= 1; i--){
-						index = (i-1)*sizeof(Registro);
-						arquivo.seekg(HEADER_OFFSET + index); //Move ponteiro para último registro;
+						index_next = (i-1)*sizeof(Registro);
+						arquivo.seekg(HEADER_OFFSET + index_next); //Move ponteiro para último registro;
 					    arquivo.read((char*)buff_extra, sizeof(Registro)); 
 						arquivo.sync();
 
 						if(buff_extra->status != OCUPADO){ //Se for um local vago; 
-							arquivo.seekp(HEADER_OFFSET + index); //Move ponteiro de escrita;
+							arquivo.seekp(HEADER_OFFSET + index_next); //Move ponteiro de escrita;
+							reg->status = OCUPADO;
+							reg->anterior = pos_buffer; 
+							reg->proximo = -1;
+							arquivo.write((char*) reg, sizeof(Registro));
+							arquivo.flush();
+
+							//Retorna à posição de conflito (posição original) para atualizar o ponteiro  para a posição i
+							buffer->proximo = i-1;  
+							arquivo.seekp(HEADER_OFFSET + pos_buffer*sizeof(Registro));
+							arquivo.write((char*) buffer, sizeof(Registro));
+							arquivo.flush();
+
+							 //Limpando;
+							string clean_chave (20,'\x00');
+							string clean_valor(50,'\x00');
+							strcpy(reg->chave,clean_chave.c_str());
+							strcpy(reg->valor,clean_valor.c_str());
+							break;
+						}
+						
+					}
+                }else{ //Se swt = 1;
+                    for(i = 1; i <= TAMANHO; i++){
+						index_next = (i-1)*sizeof(Registro);
+						arquivo.seekg(HEADER_OFFSET + index_next); //Move ponteiro para último registro;
+					    arquivo.read((char*)buff_extra, sizeof(Registro)); 
+						arquivo.sync();
+
+						if(buff_extra->status != OCUPADO){ //Se for um local vago; 
+							arquivo.seekp(HEADER_OFFSET + index_next); //Move ponteiro de escrita;
 							reg->status = OCUPADO;
 							reg->anterior = pos_buffer; 
 							reg->proximo = -1;
@@ -101,11 +134,13 @@ int inserirRegistro(fstream &arquivo, Registro *reg){
 						
 					}
 
-					delete buff_extra;
+                }
 
+					delete buff_extra;
+                
 
 				}else{	//Caso a posição atual não seja dona do lugar e precise ceder a quem tem direito;
-					long long int i, index;
+					long long int i, index_next;
 
 					//Fazer Swap entre registros;
 					Registro *buff_extra = new Registro("", "", -1, -1, VAZIO);
@@ -121,15 +156,15 @@ int inserirRegistro(fstream &arquivo, Registro *reg){
 					//Procura e move buff_extra para uma nova posição e atualiza o apontador do item anterior à buff_extra na lista;
 					//Se não houver posição sobrando, não executa a inserção
 
-
-					for(i= TAMANHO; i>=1; i--){
-						index = (i-1)*sizeof(Registro);
-						arquivo.seekg(HEADER_OFFSET + index);
+                    if(!swt){
+					  for(i = TAMANHO; i>=1; i--){
+						index_next = (i-1)*sizeof(Registro);
+						arquivo.seekg(HEADER_OFFSET + index_next);
 						arquivo.read((char*)buffer, sizeof(Registro));
 						arquivo.sync();
 
 						if(buffer->status != OCUPADO){ //Se posição estiver vazia, então escreve nela;
-							arquivo.seekp(HEADER_OFFSET + index);
+							arquivo.seekp(HEADER_OFFSET + index_next);
 							//buff_extra->status = OCUPADO;
 							arquivo.write((char*)buff_extra, sizeof(Registro));
 							arquivo.flush();
@@ -164,14 +199,57 @@ int inserirRegistro(fstream &arquivo, Registro *reg){
 
 
 					}
+                }else{
+            	  for(i = 1; i <= TAMANHO; i++){
+						index_next = (i-1)*sizeof(Registro);
+						arquivo.seekg(HEADER_OFFSET + index_next);
+						arquivo.read((char*)buffer, sizeof(Registro));
+						arquivo.sync();
 
-					delete buff_extra;
+						if(buffer->status != OCUPADO){ //Se posição estiver vazia, então escreve nela;
+							arquivo.seekp(HEADER_OFFSET + index_next);
+							//buff_extra->status = OCUPADO;
+							arquivo.write((char*)buff_extra, sizeof(Registro));
+							arquivo.flush();
+
+							//Atualizar o registro que apontava para buff_extra;
+
+							arquivo.seekg(HEADER_OFFSET + buff_extra->anterior*sizeof(Registro));
+							arquivo.read((char*)buffer, sizeof(Registro));
+							arquivo.sync();
+
+							buffer->proximo = i-1;
+							arquivo.seekp(HEADER_OFFSET + buff_extra->anterior*sizeof(Registro));
+							arquivo.write((char*)buffer, sizeof(Registro));
+							arquivo.flush();
+
+						
+						
+								//Escreve reg na posição correta;
+							arquivo.seekp(HEADER_OFFSET + hash_calculado*sizeof(Registro));
+							reg->status = OCUPADO;
+							arquivo.write((char*) reg , sizeof(Registro));
+							arquivo.flush();
+
+							//Limpando;
+							string clean_chave (20,'\x00');
+							string clean_valor(50,'\x00');
+							strcpy(reg->chave,clean_chave.c_str());
+							strcpy(reg->valor,clean_valor.c_str());
+							break;
+						
+						}
+                    }
+                }    
+                    delete buff_extra;
 				}
 
 
 
 		}else{ //Caso status = VAZIO 
 			reg->status = OCUPADO;
+            reg->anterior = -1;
+            reg->proximo = -1;
 			arquivo.seekp(HEADER_OFFSET + hash_calculado*sizeof(Registro));
 			arquivo.write((char*) reg, sizeof(Registro));
 			arquivo.flush();
@@ -181,14 +259,11 @@ int inserirRegistro(fstream &arquivo, Registro *reg){
 			string clean_valor(50,'\x00');
 			strcpy(reg->chave,clean_chave.c_str());
 			strcpy(reg->valor,clean_valor.c_str());
+            reg->anterior = -1;
+            reg->proximo = -1;
+            reg->status = VAZIO;
 		}
-
-
-
-		
 		delete buffer;
-		//Valor a ser substituído
-		return 1;
 }
 
 
@@ -201,41 +276,65 @@ int inserirRegistro(fstream &arquivo, Registro *reg){
 //Protótipo 1 (apaga mesmo os ponteiros anterior e proximo) -> para casos onde não há conflito de chaves
 void removerRegistro(fstream &arquivo, const char* chave){
 		Registro *buffer = new Registro("", "", -1, -1, VAZIO);
+        Registro* buff_extra = new Registro("","",-1,-1,VAZIO);
 
 		//Variáveis locais auxiliares para os índices de registros, bem como valores calculados de hash;
-		unsigned long long int hash = HashString(chave);
-		unsigned long long int hash_swap, index, index_swap;
+		 long long int hash = HashString(chave);
+		 long long int hash_swap, posicao, index_next, index_swap;
 
 		//Será utilizado para "limpar" strings do registro
 		string clean_chave(20,'\x00');
 		string clean_valor(50,'\x00');
 
 		do{
-			arquivo.seekg(HEADER_OFFSET + hash*sizeof(Registro));
+			arquivo.seekg(HEADER_OFFSET + hash * sizeof(Registro));
 			arquivo.read((char*)buffer, sizeof(Registro));
 			arquivo.sync();
+
 			hash_swap = hash;  //Salva o valor atual do hash para apagar registro;
 			hash = buffer->proximo;	
-			if(!strcmp(buffer->chave, chave)){ //Se a chave na posição for igual a que estamos procurando
-				
-				  //Verificar se o registro a ser apagado tem valor "proximo" != -1:
+
+            if(!strcmp(buffer->chave, chave)){ //Se a chave na posição for igual a que estamos procurando
+				        //Verificar se o registro a ser apagado tem valor "proximo" != -1:
 						//I) caso tenha, desloca esse proximo para a posição-raiz e apaga a posicao antiga do "proximo"
 						//II) caso nao tenha, basta apagar direto
 					
-					index = hash*sizeof(Registro);
+					index_next = hash * sizeof(Registro);
 					index_swap = hash_swap * sizeof(Registro);
 
-					if(buffer->proximo != -1){ //Se buffer->anterior != -1 (existe ao menos 1 registro em seguida)
+
+					if(buffer->proximo != -1){ //Se buffer->proximo != -1 (existe ao menos 1 registro em seguida)
 						
 						//Aqui será realizado o swap de registros e apagado o desejado;
-						Registro* buff_extra = new Registro("","",-1,-1,VAZIO);
-						arquivo.seekg(HEADER_OFFSET + index); //Lê o "proximo" registro
+						
+						arquivo.seekg(HEADER_OFFSET + index_next); //Lê o "proximo" registro
 						arquivo.read((char*)buff_extra, sizeof(Registro)); //e armazena em buff_extra;
 						arquivo.sync();
+                        
+
+                        //Atualizando o ponteiro "anterior":
+                        buff_extra->anterior = buffer->anterior;
 
 						arquivo.seekp(HEADER_OFFSET + index_swap); //Vai para a posição original para inserir o "proximo"
 						arquivo.write((char*)buff_extra, sizeof(Registro)); //registro nela (swap)
 						arquivo.flush();
+                        
+
+                        
+                        posicao = buff_extra->proximo; 
+
+                        //se buff_extra->proximo != 1, então podemos rearranjar com o elemento seguinte;
+                        if(buff_extra->proximo != -1){
+                            arquivo.seekg(HEADER_OFFSET + (posicao * sizeof(Registro)));
+                            arquivo.read((char*)buff_extra, sizeof(Registro));
+                            arquivo.sync();
+                        
+                            buff_extra->anterior = hash_swap; //Atualiza ponteiro "anterior" do registro seguinte ao que foi reordenado
+                            arquivo.seekp(HEADER_OFFSET + (posicao * sizeof(Registro)));
+                            arquivo.write((char*)buff_extra, sizeof(Registro));
+                            arquivo.flush();
+                        }
+                        
 
 						//Preenchendo com 0's os bytes de chave e valor;
 						strcpy(buffer->chave,clean_chave.c_str());
@@ -248,39 +347,59 @@ void removerRegistro(fstream &arquivo, const char* chave){
 						
 						
 						//Apagando o registro antigo de "proximo" (finalizando swap)
-						arquivo.seekp(HEADER_OFFSET + index); //Vai para a posição "proximo" para finalizar o swap
+						arquivo.seekp(HEADER_OFFSET + index_next); //Vai para a posição "proximo" para finalizar o swap
 						arquivo.write((char*)buffer, sizeof(Registro));
 						arquivo.flush();
 
-						//Atualiza hash e elimina buffer extra;
-						hash = hash_swap;
-						delete buff_extra;
-					
+						//Atualiza hash com  o valor original(valor em que !strcmp foi true) e elimina buffer extra;
+
+                        hash = hash_swap;
+
 					}else{
 						//Caso o valor de "proximo" == -1 (registro no fim da lista encadeada);
 							//Preenchendo com 0's os bytes de chave e valor;
 						strcpy(buffer->chave,clean_chave.c_str());
 						strcpy(buffer->valor,clean_valor.c_str());
 						
-						//Valores-padrão de um registro vazio;
-						buffer->anterior = -1;
-						buffer->proximo = -1;
-						buffer->status = VAZIO;
-						
-						
-						//Apagando o registro antigo de "proximo" (finalizando swap)
-						arquivo.seekp(HEADER_OFFSET + index); //Vai para a posição "proximo" para finalizar o swap
-						arquivo.write((char*)buffer, sizeof(Registro));
-						arquivo.flush();
+                        
+
+                        //Se existir algum registro apontando para o que vai ser excluído (este for o último de uma lista com > 1 elemento):
+                        if(buffer->anterior != -1){
+                            arquivo.seekg(HEADER_OFFSET + buffer->anterior*sizeof(Registro));
+                            arquivo.read((char*)buff_extra, sizeof(Registro));
+                            arquivo.sync();
+
+                            buff_extra->proximo = -1; 
+                            
+                            arquivo.seekp(HEADER_OFFSET + buffer->anterior*sizeof(Registro));
+                            arquivo.write((char*)buff_extra, sizeof(Registro));
+                            arquivo.flush();
+
+                            buffer->anterior = -1;
+						    buffer->proximo = -1;
+						    buffer->status = VAZIO;
+                		    arquivo.seekp(HEADER_OFFSET + index_next); //Vai para a posição "proximo" para finalizar o swap
+                            arquivo.write((char*)buffer, sizeof(Registro));
+						    arquivo.flush();
+
+                        }else{ //Caso seja um elemento único;
+                                       
+                            buffer->anterior = -1;
+						    buffer->proximo = -1;
+						    buffer->status = VAZIO;
+                            arquivo.seekp(HEADER_OFFSET + index_swap); //Vai para a posição "proximo" para finalizar o swap
+                            arquivo.write((char*)buffer, sizeof(Registro));
+						    arquivo.flush();
+                        }
 
 					}
-
 			}
 			
 		}while(hash != -1);
 
 
 	delete buffer;
+    delete buff_extra;
 }
 
 
@@ -308,6 +427,7 @@ void consultarRegistro(fstream &arquivo, const char* chave){
 
   //APAGUE ESTE MÉTODO (É SOMENTE PARA DEBUG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!);
 //Exibe todos os registros
+
 /*void exibirRegistros(fstream &arquivo){
 	Registro* buffer = new Registro("","",-1,-1,VAZIO);
 
@@ -380,8 +500,6 @@ int main(int argc, char* argv[]){
 		for(int i = 0 ; i < TAMANHO; i++){
 			arquivo.write((char*)regist, sizeof(Registro)); //Escreve 'TAMANHO' vezes no arquivo vazio (cada posição armazena um único registro neste caso de linear probing);
 		}
-		//REMOVER ESTE COUT!!!!!!!!!!!!!!!!!!!!!
-		cout << "Arquivo preenchido " << sizeof(Registro) << endl;
 	}
 
 	
@@ -395,6 +513,7 @@ int main(int argc, char* argv[]){
 	//unsigned int buffer_chave; //Armazena os valores de chave provisoriamente (usado em consulta);
 	char opcao = 'm'; //Armazena opcao do menu ('m' é um valor aleatório de inicializacao);
 	char entrada[100]; //Armazena o nome no caso de inserção (variável auxiliar p/ filtragem da quantidade de caracteres);
+    bool swt = 0;
 
 	while(opcao != 'e'){  //Laço de repetição do menu
 		
@@ -405,10 +524,7 @@ int main(int argc, char* argv[]){
 
 		switch(opcao){
 			//////////////////////////////////////////////////////////////////////////////////////
-				case 'i': {
-					
-							
-							
+				case 'i': { //Inserção;
 							cin.getline(entrada,100);
 							
 							if(cin.gcount() < 20){   //se forem < (20 caracteres + '\n')
@@ -425,11 +541,9 @@ int main(int argc, char* argv[]){
 								strncpy(regist->valor,entrada,50);	  //Caso o nome tenha mais de 20 caracteres, pega
 							}										//somente os 20 primeiros
 							
-							
-							
-							
 							//tenta inserir novo registro na tabela
-							inserirRegistro(arquivo, regist);
+							inserirRegistro(arquivo, regist, swt);
+                            swt = !swt; //Alterna valor de swt (switch)
 							break;
 							
 						  }
@@ -458,11 +572,11 @@ int main(int argc, char* argv[]){
 						}
 
 					//DEBUG - REMOVER DEPOIS
-					/*case 'p':{  //Opcao de print dos registros;
+					case 'p':{  //Opcao de print dos registros;
 							exibirRegistros(arquivo);
 							cout << "//////////////////////////////////////////" << endl;
 							break;	
-						}*/
+						}
 			//////////////////////////////////////////////////////////////////////////////////////			
 				case 'e':{ //Opcao de fim de programa;
 							break;	
